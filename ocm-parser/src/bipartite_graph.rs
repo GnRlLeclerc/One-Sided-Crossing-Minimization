14620fd828476e0c4bd4cfa04124e9c80627aea5
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-02-22 17:51:31 +0100

    immplement function to write BipartiteGraphs back to file system

--- ocm-parser/src/bipartite_graph.rs
+++ ocm-parser/src/bipartite_graph.rs
@@ -1,5 +1,6 @@
 //! Basic bipartite graph definition, to be parsed from the dataset files
 
+use std::io::Write;
 /// Bipartite Graph data structure, as parsed from the dataset files
 #[derive(Debug, Default, Clone)]
 pub struct BipartiteGraph {
@@ -16,5 +17,26 @@ impl BipartiteGraph {
     /// Create a new empty graph
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn save_to_file(&self, filename: &str) -> std::io::Result<()> {
+        let mut file = std::fs::File::create(filename)?;
+        let mut writer = std::io::BufWriter::new(&mut file);
+
+        // Write the header
+        writeln!(
+            writer,
+            "p ocr {} {} {}",
+            self.top_node_count,
+            self.bottom_node_count,
+            self.edges.len()
+        )?;
+
+        // Write the edges
+        for (top, bottom) in &self.edges {
+            writeln!(writer, "{} {}", top, bottom)?;
+        }
+
+        Ok(())
     }
 }
